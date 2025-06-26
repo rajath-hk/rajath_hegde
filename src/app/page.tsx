98601fe66a1dc7d8f9d87548cb8de8441630f58e
rajath hegde <138708414+rajath-hk@users.noreply.github.com>
2025-06-26 12:13:33 +0530

    clearing 1

--- src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import OsUi from "@/components/retro-os";
-
-export default function Home() {
-  return (
-    <main>
-      <OsUi />
-    </main>
-  );
-}
--- src/components/ui/sidebar.tsx
+++
@@ -1,1 +0,0 @@
-// This component is not used in the project and has been removed to clean up the codebase.
