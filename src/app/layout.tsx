34440f047fd3cd26ce1810689c15a346da7210b5
rajath hegde <138708414+rajath-hk@users.noreply.github.com>
2025-06-26 14:11:58 +0530

    v2

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { ThemeProvider } from '@/components/theme-provider';
 
 export const metadata: Metadata = {
-  title: 'RetroFolio: An Interactive Portfolio',
+  title: 'Rajath Hegde',
   description: "An interactive portfolio with a retro desktop theme.",
 };
 
